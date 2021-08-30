ad40df115417d24afa682e1c75df6e9c1a755124
lzn01 <408135242@qq.com>
2021-08-30 21:56:08 +0800

    设置grids布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,13 @@ export const Home = () => {
     return (
         <div className="home">
             <header style={{backgroundImage: `url(${headerBg})`}}/>
+            <main>
+                <section className="section1"/>
+                <section className="section2"/>
+                <section className="section3"/>
+                <section className="section4"/>
+                <section className="section5"/>
+            </main>
         </div>
     );
 };
