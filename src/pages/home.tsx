5215f17fb3347c618c3a27c4fa830965e353f5f0
lzn01 <408135242@qq.com>
2021-09-02 14:05:26 +0800

    完成chart5

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import {Chart1} from "../component/chart-1";
 import {Chart2} from "../component/chart-2";
 import {Chart3} from "../component/chart-3";
 import {Chart4} from "../component/chart-4";
+import {Chart5} from "../component/chart-5";
 
 export const Home = () => {
     const year = new Date().getFullYear();
@@ -20,7 +21,9 @@ export const Home = () => {
                     <Chart3/>
                     <Chart4/>
                 </section>
-                <section className="bordered section3"/>
+                <section className="bordered section3">
+                    <Chart5/>
+                </section>
                 <section className="bordered section4"/>
                 <section className="bordered section5"/>
             </main>
