34e594f714997ca36fe0f925dba1b50c33e5b7f4
lzn01 <408135242@qq.com>
2021-09-02 14:51:25 +0800

    chart7 8 9完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import './home.scss';
 import headerBg from '../images/header.png'
 import {Chart1} from "../component/chart-1";
@@ -7,6 +7,9 @@ import {Chart3} from "../component/chart-3";
 import {Chart4} from "../component/chart-4";
 import {Chart5} from "../component/chart-5";
 import {Chart6} from "../component/chart-6";
+import {Chart7} from "../component/chart-7";
+import {Chart8} from "../component/chart-8";
+import {Chart9} from "../component/chart-9";
 
 export const Home = () => {
     const year = new Date().getFullYear();
@@ -25,8 +28,16 @@ export const Home = () => {
                 <section className="bordered section3">
                     <Chart5/>
                 </section>
-                <section className="bordered section4">
+                <section className="section4">
                     <Chart6/>
+                    <div className="bordered 年龄段">
+                        <h2>犯罪人员年龄段分布</h2>
+                        <div className="charts">
+                            <Chart7/>
+                            <Chart8/>
+                            <Chart9/>
+                        </div>
+                    </div>
                 </section>
                 <section className="bordered section5"/>
             </main>
